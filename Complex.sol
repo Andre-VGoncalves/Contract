39f8938ec39dbfc71625c73b1abff558ab103811
Andre Vieira Goncalves <andrevieira@brq.com>
2017-11-21 13:45:04 -0200

    function data add

--- Complex.sol
+++ Complex.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.4.0;
 contract Complex {
     struct Data {
         uint a;
-        bytes32 b;
+        bytes3 b;
         mapping (uint => uint) map;
     }
     mapping (uint => mapping(bool => data[])) public data;
+    
+    function data (uint arg1, bool arg2, uint arg3) returns (uint a, bytes3 b) {
+        a = data[arg1][arg2][arg3].a;
+        b = data[arg1][arg2][arg3].b;
+    } 
 }

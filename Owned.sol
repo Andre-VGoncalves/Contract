c1a9a5c02297518a43215042d0f5e2c34484d058
Andre Vieira Goncalves <andrevieira@brq.com>
2017-11-21 14:31:39 -0200

    finish code

--- Owned.sol
+++ Owned.sol
@@ -9,9 +9,50 @@ contract owned {
     }
 
 }
-//teste
+
 contract mortal is owned {
     function close() onlyOwner {
         selfdestruct(owner);
     }
 }
+
+contract priced {
+    modifier costs (uint price){
+        if (msg.value >= price){
+            _;
+        }
+    }
+}
+
+contract register is priced, owned {
+    mapping (address => bool) registeredAddress;
+    uint price;
+
+    function Register() payble costs(price) {
+        registeredAddress[msg.sender] = true;
+    }
+
+    function changePrice (uint _price) OnlyOwner {
+        price = _price;
+    }
+}
+
+contract Mutex {
+    bool locked;
+    modifier noReentrancy() {
+        require(!locked);
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function f() noReentrancy returns (uint) {
+        require(msg.sender.call());
+        return 7;
+    }
+}
+
+
+
+
+
